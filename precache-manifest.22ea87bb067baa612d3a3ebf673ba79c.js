5028ef7823bc516f2751f2119ffa265f2d0e3d9a
hungrykiller <devanshubilthare821@gmail.com>
2020-06-10 02:05:02 +0530

    Updates

---
+++ precache-manifest.22ea87bb067baa612d3a3ebf673ba79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2fbe913c99c07b1fbdda19c9f54a08",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0b455756d83082b69b52",
+    "url": "/monsters-rolodex/static/css/main.d415c40c.chunk.css"
+  },
+  {
+    "revision": "2063aca658ba578437d7",
+    "url": "/monsters-rolodex/static/js/2.06610b51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.06610b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b455756d83082b69b52",
+    "url": "/monsters-rolodex/static/js/main.a8158bbb.chunk.js"
+  },
+  {
+    "revision": "28dd86c2c179fb4951be",
+    "url": "/monsters-rolodex/static/js/runtime-main.ed300d6a.js"
+  }
+]);
